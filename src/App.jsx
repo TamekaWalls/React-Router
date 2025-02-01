230cf12d8a5f89bbae714f431a94cfdb1e2472d5
Tameka Walls <tamekalwalls@gmail.com>
2025-02-01 12:40:09 -0500

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import './App.css'
+import { Routes, Route, Link } from "react-router-dom"
 
 function App() {
 
   return (
     <div id="container">
-    <h1>Hello React Router!</h1>
-    <div id="navbar">{/* navigation here */}</div>
-    <div id="main-section">{/* routes here */}</div>
-  </div>
+      <h1>Hello React Router!</h1>
+      <div id="navbar">{/* navigation here */}</div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/blue" element={<div className="blue">Blue</div>} />
+          <Route path="/red" element={<div className="red">Red</div>} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
